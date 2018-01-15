12f22a468f127a93ce08caa2a3b0d80978d74b32
Pete Cooper <petecooper@users.noreply.github.com>
2018-01-15 18:10:15 +0000

    More stuff to do

--- aptgetdistupgradeclean.sh
+++ aptgetdistupgradeclean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y autoclean
+sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoclean && sudo apt-get -y autoremove --purge && sudo apt-get -y clean
