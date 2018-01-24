96d75ede1779756c1254bc97ccbf3cb9b2d82c3a
Pete Cooper <pete@pragmatika.net>
2018-01-24 16:58:38 +0000

    Prettier dates

--- varwwwzipapacheconftohome.sh
+++ varwwwzipapacheconftohome.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo zip --symlinks -r ~/"$(date +"%Y%m%d")-$(hostname)-var-www-apache.zip" /var/www /etc/apache2/
+sudo zip --symlinks -r ~/"$(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www-apache.zip" /var/www /etc/apache2/
