8b781886dfc56f8b30d0d4f80b95f943a744b203
Pete Cooper <pete@pragmatika.net>
2018-05-20 22:02:13 +0100

    Tidy up
    Signed-off-by: Pete Cooper <pete@pragmatika.net>

--- __fetchscripts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -f ~/apachebounce.sh ~/aptgetdistupgradeclean.sh ~/letsencryptrenew.sh ~/varwwwnginxconfziptohome.sh ~/varwwwziptohome.sh ~/aptgetdistupgradecleanreboot.sh ~/aptgetdistupgradecleanshutdown.sh ~/nginxbounce.sh ~/varwwwzipapacheconftohome.sh
-git clone https://github.com/petecooper/sysadmin-scripts.git ~/sysadmin-scripts/ && mv ~/sysadmin-scripts/*.sh ~ && rm -rf ~/sysadmin-scripts
