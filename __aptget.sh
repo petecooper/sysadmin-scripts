4a54ca4683d0cc9c30e64c833e487f3dc7670e66
Pete Cooper <pete@pragmatika.net>
2018-09-09 21:47:19 +0100

    rejig: `apt-get` -> `apt`
    Signed-off-by: Pete Cooper <pete@pragmatika.net>

--- __aptget.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoclean && sudo apt-get -y autoremove --purge && sudo apt-get -y clean
---
+++ __aptupgradeshutdown.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt update && sudo apt -y dist-upgrade && sudo apt -y autoclean && sudo apt -y autoremove --purge && sudo apt -y clean && sudo shutdown -h now
