0ab6577fc4532dc7c86b7c05304a1ea3ee375606
Pete Cooper <petecooper@users.noreply.github.com>
2019-01-23 09:57:28 +0000

    Update __aptupgradereboot.sh

--- __aptupgradereboot.sh
+++ __aptupgradereboot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update -o Acquire::http::AllowRedirect=false && sudo apt -o Acquire::http::AllowRedirect=false -y dist-upgrade && sudo apt -y autoclean && sudo apt -y autoremove --purge && sudo apt -y clean && sudo reboot
+sudo apt update && sudo apt -y dist-upgrade && sudo apt -y autoclean && sudo apt -y autoremove --purge && sudo apt -y clean && sudo reboot
