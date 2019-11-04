3844b9238b494ae3706d0d3845f8dd7810b2679f
Pete Cooper <petecooper@users.noreply.github.com>
2019-11-04 11:04:00 +0000

    Update __certbotteardownrenew.sh

--- __certbotteardownrenew.sh
+++ __certbotteardownrenew.sh
@@ -3,4 +3,4 @@ sudo rm -rf /opt/eff.org/certbot/venv/
 sudo rm -rf /opt/letsencrypt/
 sudo git clone --depth 1 https://github.com/certbot/certbot /opt/letsencrypt
 sudo /opt/letsencrypt/certbot-auto renew
-sudo nginx -t && systemctl reload nginx
+sudo nginx -t && sudo systemctl reload nginx
