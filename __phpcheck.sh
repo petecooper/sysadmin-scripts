3ae8e5b69f7b5888df634591fc3225b16d1b0d6b
Pete Cooper <petecooper@users.noreply.github.com>
2020-03-31 16:29:39 +0100

    Update __phpcheck.sh

--- __phpcheck.sh
+++ __phpcheck.sh
@@ -2,63 +2,57 @@
 echo -e "******************************"
 echo -e "* Starting legacy PHP checks *"
 echo -e "******************************"
-echo -e "==> Checking for PHP 7.0..."
 /etc/php/7.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
   php70ver=$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')
-  echo -e "Yes: "$php70ver
+  echo -e "PHP "$php70ver" installed"
 else
   echo -e "No"
 fi
-echo -e "==> Checking for PHP 7.1..."
 /etc/php/7.1/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
   php71ver=$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')
-  echo -e "Yes: "$php71ver
+  echo -e "PHP "$php71ver" installed"
 else
   echo -e "No"
 fi
-echo -e "==> Checking for PHP 7.2..."
 /etc/php/7.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
   php72ver=$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')
-  echo -e "Yes: "$php72ver
+  echo -e "PHP "$php72ver" installed"
 else
   echo -e "No"
 fi
 echo -e "********************************"
 echo -e "* Starting mainline PHP checks *"
 echo -e "********************************"
-echo -e "==> Checking for PHP 7.3..."
 /etc/php/7.3/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
   php73ver=$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')
-  echo -e "Yes: "$php73ver
+  echo -e "PHP "$php73ver" installed"
 else
   echo -e "No"
 fi
-echo -e "==> Checking for PHP 7.4..."
 /etc/php/7.4/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
   php74ver=$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')
-  echo -e "Yes: "$php74ver
+  echo -e "PHP "$php74ver" installed"
 else
   echo -e "No"
 fi
 echo -e "***********************************"
 echo -e "* Starting development PHP checks *"
 echo -e "***********************************"
-echo -e "==> Checking for PHP source..."
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
   phpsrcver=$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')
-  echo -e "Yes: "$phpsrcver
+  echo -e "PHP "$phpsrcver" installed"
 else
   echo -e "No"
 fi
