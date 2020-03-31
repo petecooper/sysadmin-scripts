2b71c27ef4babdad939f33393e6f32575daf825a
Pete Cooper <petecooper@users.noreply.github.com>
2020-03-31 16:16:18 +0100

    Update __phpcheck.sh

--- __phpcheck.sh
+++ __phpcheck.sh
@@ -1,49 +1,58 @@
 #!/bin/bash
-echo "Checking for PHP 7.0..."
+echo -e "******************************"
+echo -e "* Starting legacy PHP checks *"
+echo -e "******************************"
+echo -e "==> Checking for PHP 7.0..."
 /etc/php/7.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo "Yes"
+  echo -e "Yes"
 else
-  echo "No"
+  echo -e "No"
 fi
-echo "Checking for PHP 7.1..."
+echo -e "==> Checking for PHP 7.1..."
 /etc/php/7.1/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo "Yes"
+  echo -e "Yes"
 else
-  echo "No"
+  echo -e "No"
 fi
-echo "Checking for PHP 7.2..."
+echo -e "==> Checking for PHP 7.2..."
 /etc/php/7.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo "Yes"
+  echo -e "Yes"
 else
-  echo "No"
+  echo -e "No"
 fi
-echo "Checking for PHP 7.3..."
+echo -e "********************************"
+echo -e "* Starting mainline PHP checks *"
+echo -e "********************************"
+echo -e "==> Checking for PHP 7.3..."
 /etc/php/7.3/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo "Yes"
+  echo -e "Yes"
 else
-  echo "No"
+  echo -e "No"
 fi
-echo "Checking for PHP 7.4..."
+echo -e "==> Checking for PHP 7.4..."
 /etc/php/7.4/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo "Yes"
+  echo -e "Yes"
 else
-  echo "No"
+  echo -e "No"
 fi
-echo "Checking for PHP source..."
+echo -e "***********************************"
+echo -e "* Starting development PHP checks *"
+echo -e "***********************************"
+echo -e "==> Checking for PHP source..."
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo "Yes"
+  echo -e "Yes"
 else
-  echo "No"
+  echo -e "No"
 fi
