63e22287049c923ff0987e044a3494b46cffc11e
Pete Cooper <petecooper@users.noreply.github.com>
2020-03-31 16:10:52 +0100

    Update __phpcheck.sh

--- __phpcheck.sh
+++ __phpcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Checking for PHP 7.1..."
-/etc/php/7.1/sbin/php-fpm -v > /dev/null
+/etc/php/7.1/sbin/php-fpm -v 2> /dev/null
 if [ $? -eq 0 ]
 then
   echo "Yes"
@@ -8,7 +8,7 @@ else
   echo "No"
 fi
 echo "Checking for PHP 7.2..."
-/etc/php/7.2/sbin/php-fpm -v > /dev/null
+/etc/php/7.2/sbin/php-fpm -v 2> /dev/null
 if [ $? -eq 0 ]
 then
   echo "Yes"
@@ -16,7 +16,7 @@ else
   echo "No"
 fi
 echo "Checking for PHP 7.3..."
-/etc/php/7.3/sbin/php-fpm -v > /dev/null
+/etc/php/7.3/sbin/php-fpm -v 2> /dev/null
 if [ $? -eq 0 ]
 then
   echo "Yes"
@@ -24,7 +24,15 @@ else
   echo "No"
 fi
 echo "Checking for PHP 7.4..."
-/etc/php/7.4/sbin/php-fpm -v > /dev/null
+/etc/php/7.4/sbin/php-fpm -v 2> /dev/null
+if [ $? -eq 0 ]
+then
+  echo "Yes"
+else
+  echo "No"
+fi
+echo "Checking for PHP source..."
+/etc/php/php-src/sbin/php-fpm -v 2> /dev/null
 if [ $? -eq 0 ]
 then
   echo "Yes"
