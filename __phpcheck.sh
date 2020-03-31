d1b8a1fcaab08c36f55fd1323e0f567443f7c721
Pete Cooper <petecooper@users.noreply.github.com>
2020-03-31 16:01:16 +0100

    Update __phpcheck.sh

--- __phpcheck.sh
+++ __phpcheck.sh
@@ -2,9 +2,9 @@
 /etc/php/7.3/sbin/php-fpm -v
 if [ $? -eq 0 ]
 then
-  echo "[\xE2\x9C\x94] PHP 7.3"
+  echo "[\xE2\x9C\x93] PHP 7.3"
   exit 0
 else
-  echo "[X] PHP 7.3" >&2
+  echo "[\xE2\x9C\x97] PHP 7.3" >&2
   exit 1
 fi
