31e55354d89a6cb7d211359c0bab64d31476fb11
Pete Cooper <petecooper@users.noreply.github.com>
2020-04-13 15:48:46 +0100

    Update __phpversioncheck.sh

--- __phpversioncheck.sh
+++ __phpversioncheck.sh
@@ -2,36 +2,36 @@
 /etc/php/7.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  php70ver=$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')
-  echo -e "PHP "$php70ver" installed"
+  php_70_ver=$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')
+  echo -e "PHP "$php_70_ver" installed"
 fi
 /etc/php/7.1/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  php71ver=$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')
-  echo -e "PHP "$php71ver" installed"
+  php_71_ver=$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')
+  echo -e "PHP "$php_71_ver" installed"
 fi
 /etc/php/7.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  php72ver=$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')
-  echo -e "PHP "$php72ver" installed"
+  php_72_ver=$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')
+  echo -e "PHP "$php_72_ver" installed"
 fi
 /etc/php/7.3/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  php73ver=$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')
-  echo -e "PHP "$php73ver" installed"
+  php_73_ver=$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')
+  echo -e "PHP "$php_73_ver" installed"
 fi
 /etc/php/7.4/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  php74ver=$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')
-  echo -e "PHP "$php74ver" installed"
+  php_74_ver=$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')
+  echo -e "PHP "$php_74_ver" installed"
 fi
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  phpsrcver=$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')
-  echo -e "PHP "$phpsrcver" installed"
+  php_src_ver=$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')
+  echo -e "PHP "$php_src_ver" installed"
 fi
