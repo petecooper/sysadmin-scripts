39e1fe54ba6c6a9a9be7192faf861098a9b22776
Pete Cooper <petecooper@users.noreply.github.com>
2020-05-05 09:04:43 +0100

    Create __swapmakereboot.sh

---
+++ __swapmakereboot.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+echo 'Approximate system RAM (GB): '$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs -I {} echo "scale=4; {}/1024^2" | bc | xargs printf "%.2f") \
+&& read -p 'Swapfile size with unit (e.g. 4G): ' swapfilesize \
+&& sudo fallocate -l $swapfilesize /swapfile \
+&& sudo chmod 600 /swapfile \
+&& sudo mkswap /swapfile \
+&& sudo swapon /swapfile \
+&& sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab' \
+&& echo -e '**********************************' \
+&& echo -e '* Swapfile created. Rebooting... *' \
+&& echo -e '**********************************' \
+&& sudo reboot
