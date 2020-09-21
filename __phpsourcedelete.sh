7d4a2d23bbddcbacf9aa543966b30756466d28c5
Pete Cooper <petecooper@users.noreply.github.com>
2020-09-21 11:52:22 +0100

    Update and rename __phpdeletesource.sh to __phpsourcedelete.sh

--- __phpsourcedelete.sh
+++ __phpsourcedelete.sh
@@ -5,5 +5,7 @@ rm -rf ~/php73-source.tar.gz
 rm -rf ~/php73-source/
 rm -rf ~/php74-source.tar.gz
 rm -rf ~/php74-source/
+rm -rf ~/php80-source.tar.gz
+rm -rf ~/php80-source/
 rm -rf ~/php-src-source.tar.gz
 rm -rf ~/php-src-source/
