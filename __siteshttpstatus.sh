fc86527a47b36c4155d43e023ae95a86989df6db
Pete Cooper <petecooper@users.noreply.github.com>
2020-10-01 14:17:29 +0100

    Update __siteshttpstatus.sh

--- __siteshttpstatus.sh
+++ __siteshttpstatus.sh
@@ -2,7 +2,5 @@
 while read site; \
 do printf "$site" \
 && printf ": " \
-&& curl -Is https://"$site" | \
-grep "^HTTP" | \
-cat; \
+&& curl -Is https://"$site" | grep "^HTTP" | cat; \
 done < /var/www/sites/sites.txt
