1733a1c23cd5adc77ac2691da4dd7e99eaa33ff7
Pete Cooper <petecooper@users.noreply.github.com>
2020-12-13 16:30:23 +0000

    Update __apt-full-upgrade-cleanup-shutdown.sh
    formatting

--- __apt-full-upgrade-cleanup-shutdown.sh
+++ __apt-full-upgrade-cleanup-shutdown.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo apt update && sudo apt -y full-upgrade && sudo apt -y autoclean && sudo apt -y autoremove --purge && sudo apt -y clean && sudo shutdown -h now
+sudo apt update \
+&& sudo apt -y full-upgrade \
+&& sudo apt -y autoclean \
+&& sudo apt -y autoremove --purge \
+&& sudo apt -y clean \
+&& sudo shutdown -h now
