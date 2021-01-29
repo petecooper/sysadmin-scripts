c297c9d5ccaf83d1996c5bc5cb7965b7df7e1ea0
Pete Cooper <petecooper@users.noreply.github.com>
2021-01-29 09:34:09 +0000

    Update __certbot-renew.sh

--- __certbot-renew.sh
+++ __certbot-renew.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo /opt/certbot/certbot renew --config-dir /etc/certbot/
+if [ -f "/opt/certbot/certbot" ]; then
+  sudo /opt/certbot/certbot renew --config-dir /etc/certbot/ && exit
+fi
+if [ -f "/usr/bin/certbot" ]; then
+  sudo /usr/bin/certbot renew --config-dir /etc/certbot/ && exit
+fi
