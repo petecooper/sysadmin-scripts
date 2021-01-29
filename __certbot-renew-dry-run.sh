56fc57f7d89131808d90c76c67eb80a9583a87b1
Pete Cooper <petecooper@users.noreply.github.com>
2021-01-29 09:35:38 +0000

    Update __certbot-renew-dry-run.sh

--- __certbot-renew-dry-run.sh
+++ __certbot-renew-dry-run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo /opt/certbot/certbot renew --config-dir /etc/certbot/ --dry-run
+if [ -f "/opt/certbot/certbot" ]; then
+  sudo /opt/certbot/certbot renew --config-dir /etc/certbot/ --dry-run && exit
+fi
+if [ -f "/usr/bin/certbot" ]; then
+  sudo /usr/bin/certbot renew --config-dir /etc/certbot/ --dry-run && exit
+fi
