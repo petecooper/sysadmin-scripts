ccd0504277907942dbc2915a4b775b376e030f14
Pete Cooper <petecooper@users.noreply.github.com>
2021-01-29 09:34:46 +0000

    Update __certbot-renew-force.sh

--- __certbot-renew-force.sh
+++ __certbot-renew-force.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo /opt/certbot/certbot renew --config-dir /etc/certbot/ --force-renew
+if [ -f "/opt/certbot/certbot" ]; then
+  sudo /opt/certbot/certbot renew --config-dir /etc/certbot/ --force-renew && exit
+fi
+if [ -f "/usr/bin/certbot" ]; then
+  sudo /usr/bin/certbot renew --config-dir /etc/certbot/ --force-renew && exit
+fi
