4f9538f76ec194104848eca330ea8381b659e6b2
Pete Cooper <petecooper@users.noreply.github.com>
2021-02-03 15:45:41 +0000

    Update __php-version-check.sh

--- __php-version-check.sh
+++ __php-version-check.sh
@@ -2,40 +2,40 @@
 /etc/php/7.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')" installed (7.0 binary)"
+  echo -e "PHP "$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')" installed"
 fi
 /etc/php/7.1/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')" installed (7.1 binary)"
+  echo -e "PHP "$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')" installed"
 fi
 /etc/php/7.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')" installed (7.2 binary)"
+  echo -e "PHP "$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')" installed"
 fi
 /etc/php/7.3/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')" installed (7.3 binary)"
+  echo -e "PHP "$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')" installed"
 fi
 /etc/php/7.4/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')" installed (7.4 binary)"
+  echo -e "PHP "$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')" installed"
 fi
 /etc/php/8.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/8.0/bin/php -r 'echo PHP_VERSION;')" installed (8.0 binary)"
+  echo -e "PHP "$(/etc/php/8.0/bin/php -r 'echo PHP_VERSION;')" installed"
 fi
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')" installed (php-src binary)"
+  echo -e "PHP "$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')" installed"
 fi
 /usr/local/bin/php -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/usr/local/bin/php -r 'echo PHP_VERSION;')" set up as `php` symlink"
+  echo -e "symlink set"
 fi
