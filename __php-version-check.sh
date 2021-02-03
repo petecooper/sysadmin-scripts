2d679dfec512fcf7505e66c28ea967a057f5187a
Pete Cooper <petecooper@users.noreply.github.com>
2021-02-03 15:52:04 +0000

    Update __php-version-check.sh

--- __php-version-check.sh
+++ __php-version-check.sh
@@ -2,40 +2,40 @@
 /etc/php/7.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')" installed"
+  echo -e "Found: PHP "$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')" (EOL: 10 Jan 2019)"
 fi
 /etc/php/7.1/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')" installed"
+  echo -e "Found: PHP "$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')" (EOL: 1 Dec 2019)"
 fi
 /etc/php/7.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')" installed"
+  echo -e "Found: PHP "$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')" (EOL: 30 Nov 2020)"
 fi
 /etc/php/7.3/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')" installed"
+  echo -e "Found: PHP "$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')" (EOL: 6 Dec 2021)"
 fi
 /etc/php/7.4/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')" installed"
+  echo -e "Found: PHP "$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')" (EOL: 28 Nov 2022)"
 fi
 /etc/php/8.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/8.0/bin/php -r 'echo PHP_VERSION;')" installed"
+  echo -e "Found: PHP "$(/etc/php/8.0/bin/php -r 'echo PHP_VERSION;')" (EOL: 26 Nov 2023)"
 fi
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "PHP "$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')" installed"
+  echo -e "Found: PHP "$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')
 fi
 /usr/local/bin/php -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "symlink: "$(/usr/local/bin/php -r 'echo PHP_VERSION;')
+  echo -e "`php`: PHP"$(/usr/local/bin/php -r 'echo PHP_VERSION;')
 fi
