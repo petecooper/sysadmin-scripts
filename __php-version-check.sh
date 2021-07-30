f22d8f77d45ec39cffc18310c2b3e340bb7c7b89
Pete Cooper <petecooper@users.noreply.github.com>
2021-07-30 16:31:35 +0100

    Update __php-version-check.sh

--- __php-version-check.sh
+++ __php-version-check.sh
@@ -7,48 +7,48 @@ fi
 /etc/php/7.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n7.0 CLI: PHP "$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')" (EOL)"
+  echo -e "\n7.0 PHP CLI: PHP "$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')" (EOL)"
   echo -e "7.0 SSL:" $(/etc/php/7.0/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
 /etc/php/7.1/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n7.1 CLI: PHP "$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')" (EOL)"
+  echo -e "\n7.1 PHP CLI: PHP "$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')" (EOL)"
   echo -e "7.1 SSL:" $(/etc/php/7.1/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
 /etc/php/7.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n7.2 CLI: PHP "$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')" (EOL)"
+  echo -e "\n7.2 PHP CLI: PHP "$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')" (EOL)"
   echo -e "7.2 SSL:" $(/etc/php/7.2/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
 /etc/php/7.3/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n7.3 CLI: PHP "$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')
+  echo -e "\n7.3 PHP CLI: PHP "$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')
   echo -e "7.3 SSL:" $(/etc/php/7.3/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
 /etc/php/7.4/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n7.4 CLI: PHP "$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')
+  echo -e "\n7.4 PHP CLI: PHP "$(/etc/php/7.4/bin/php -r 'echo PHP_VERSION;')
   echo -e "7.4 SSL:" $(/etc/php/7.4/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
 /etc/php/8.0/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n8.0 CLI: PHP "$(/etc/php/8.0/bin/php -r 'echo PHP_VERSION;')
+  echo -e "\n8.0 PHP CLI: PHP "$(/etc/php/8.0/bin/php -r 'echo PHP_VERSION;')
   echo -e "8.0 SSL:" $(/etc/php/8.0/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
 /etc/php/8.1/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n8.1 CLI: PHP "$(/etc/php/8.1/bin/php -r 'echo PHP_VERSION;')
+  echo -e "\n8.1 PHP CLI: PHP "$(/etc/php/8.1/bin/php -r 'echo PHP_VERSION;')
   echo -e "8.1 SSL:" $(/etc/php/8.1/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\nsrc CLI: PHP "$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')
+  echo -e "\nsrc PHP CLI: PHP "$(/etc/php/php-src/bin/php -r 'echo PHP_VERSION;')
   echo -e "src SSL:" $(/etc/php/php-src/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
 fi
