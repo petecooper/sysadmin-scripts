0e7316568e9cd826b7c56466135074b55a221f4d
Pete Cooper <pete@pragmatika.net>
2021-07-31 14:23:00 +0100

    check for dirs, then confs

--- __php-socket.sh
+++ __php-socket.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 if [ -S "/var/run/php/php-fpm74.sock" ]; then
   echo -e "Found PHP 7.4 socket at \`/var/run/php/php-fpm74.sock\`."
+  if [ -d "/etc/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm74.sock" /etc/nginx/servers-available/ | sort
+  fi
+  if [ -d "/etc/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm74.sock" /etc/nginx/sites-available/ | sort
+  fi
 fi
 if [ -S "/var/run/php/php-fpm80.sock" ]; then
   echo -e "Found PHP 8.0 socket at \`/var/run/php/php-fpm80.sock\`."
