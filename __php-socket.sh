eeab31255dd484854a55d765e1dee6910290b939
Pete Cooper <pete@pragmatika.net>
2021-07-31 14:35:18 +0100

    attaboy

--- __php-socket.sh
+++ __php-socket.sh
@@ -3,15 +3,29 @@ if [ -S "/var/run/php/php-fpm74.sock" ]; then
   echo -e "Found PHP 7.4 socket at \`/var/run/php/php-fpm74.sock\`."
   echo -e "Socket used by..."
   if [ -d "/etc/nginx/servers-available/" ]; then
-    egrep -lir "php-fpm74.sock" /etc/nginx/servers-available/ | sed 's/\/etc\/nginx\/servers-available\//->/g'
+    egrep -lir "php-fpm74.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
   fi
   if [ -d "/etc/nginx/sites-available/" ]; then
-    egrep -lir "php-fpm74.sock" /etc/nginx/sites-available/ | sed 's/\/etc\/nginx\/sites-available\//->/g'
+    egrep -lir "php-fpm74.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
   fi
 fi
 if [ -S "/var/run/php/php-fpm80.sock" ]; then
   echo -e "\nFound PHP 8.0 socket at \`/var/run/php/php-fpm80.sock\`."
+  echo -e "Socket used by..."
+  if [ -d "/etc/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm80.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  fi
+  if [ -d "/etc/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm80.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  fi
 fi
 if [ -S "/var/run/php/php-fpm81.sock" ]; then
   echo -e "\nFound PHP 8.1 socket at \`/var/run/php/php-fpm81.sock\`."
+  echo -e "Socket used by..."
+  if [ -d "/etc/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm81.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  fi
+  if [ -d "/etc/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm81.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  fi
 fi
