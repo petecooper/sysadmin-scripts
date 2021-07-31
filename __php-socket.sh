d1143bbb9ed62d53a3865f569cf853099b3c346d
Pete Cooper <pete@pragmatika.net>
2021-07-31 14:25:09 +0100

    sed

--- __php-socket.sh
+++ __php-socket.sh
@@ -2,7 +2,7 @@
 if [ -S "/var/run/php/php-fpm74.sock" ]; then
   echo -e "Found PHP 7.4 socket at \`/var/run/php/php-fpm74.sock\`."
   if [ -d "/etc/nginx/servers-available/" ]; then
-    egrep -lir "php-fpm74.sock" /etc/nginx/servers-available/ | sort
+    egrep -lir "php-fpm74.sock" /etc/nginx/servers-available/ | sort | sed -i -e 's/^/  /'
   fi
   if [ -d "/etc/nginx/sites-available/" ]; then
     egrep -lir "php-fpm74.sock" /etc/nginx/sites-available/ | sort
