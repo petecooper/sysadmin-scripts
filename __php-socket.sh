ebc72acca2cbd15077a77f778b6f1a5af7889229
Pete Cooper <pete@pragmatika.net>
2021-07-31 14:14:49 +0100

    tweak

--- __php-socket.sh
+++ __php-socket.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -S "/var/run/php/php-fpm74.sock" ]; then
-  echo -e "Found PHP 7.4 socket at `/var/run/php/php-fpm74.sock`."
+  echo -e "Found PHP 7.4 socket."
 fi
 if [ -S "/var/run/php/php-fpm80.sock" ]; then
-  echo -e "Found PHP 8.0 socket at `/var/run/php/php-fpm80.sock`."
+  echo -e "Found PHP 8.0 socket."
 fi
 if [ -S "/var/run/php/php-fpm81.sock" ]; then
-  echo -e "Found PHP 8.1 socket at `/var/run/php/php-fpm81.sock`."
+  echo -e "Found PHP 8.1 socket."
 fi
