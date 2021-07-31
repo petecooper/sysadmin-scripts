3a43b119f304ef849f86a639842792d695c7eeb8
Pete Cooper <pete@pragmatika.net>
2021-07-31 14:15:25 +0100

    perms

--- __php-socket.sh
+++ __php-socket.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -S "/var/run/php/php-fpm74.sock" ]; then
-  echo -e "Found PHP 7.4 socket."
+  sudo echo -e "Found PHP 7.4 socket at `/var/run/php/php-fpm74.sock`."
 fi
 if [ -S "/var/run/php/php-fpm80.sock" ]; then
-  echo -e "Found PHP 8.0 socket."
+  sudo echo -e "Found PHP 8.0 socket at `/var/run/php/php-fpm80.sock`."
 fi
 if [ -S "/var/run/php/php-fpm81.sock" ]; then
-  echo -e "Found PHP 8.1 socket."
+  sudo echo -e "Found PHP 8.1 socket at `/var/run/php/php-fpm81.sock`."
 fi
