9d43beb159e00809eda32f491c073b6692d399dc
Pete Cooper <pete@pragmatika.net>
2021-07-31 14:33:00 +0100

    !

--- __php-socket.sh
+++ __php-socket.sh
@@ -3,10 +3,10 @@ if [ -S "/var/run/php/php-fpm74.sock" ]; then
   echo -e "Found PHP 7.4 socket at \`/var/run/php/php-fpm74.sock\`."
   echo -e "Socket used by..."
   if [ -d "/etc/nginx/servers-available/" ]; then
-    egrep -lir "php-fpm74.sock" /etc/nginx/servers-available/ | sed -i 's/\/etc\/nginx\/servers-available\//->/g'
+    egrep -lir "php-fpm74.sock" /etc/nginx/servers-available/ | sed 's/\/etc\/nginx\/servers-available\//->/g'
   fi
   if [ -d "/etc/nginx/sites-available/" ]; then
-    egrep -lir "php-fpm74.sock" /etc/nginx/sites-available/ | sed -i 's/\/etc\/nginx\/sites-available\//->/g'
+    egrep -lir "php-fpm74.sock" /etc/nginx/sites-available/ | sed 's/\/etc\/nginx\/sites-available\//->/g'
   fi
 fi
 if [ -S "/var/run/php/php-fpm80.sock" ]; then
