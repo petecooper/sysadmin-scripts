1dae3f77c08fd09afe0a4c42fc1ec06f10d061fa
Pete Cooper <pete@pragmatika.net>
2022-04-25 15:19:59 +0000

    preflight stuff for php 8.2

--- __php-socket.sh
+++ __php-socket.sh
@@ -26,3 +26,12 @@ if [ -S "/var/run/php/php-fpm81.sock" ]; then
     egrep -lir "php-fpm81.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
   fi
 fi
+if [ -S "/var/run/php/php-fpm82.sock" ]; then
+  echo -e "\nFound PHP 8.2 socket at \`/var/run/php/php-fpm82.sock\`."
+  if [ -d "/etc/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm82.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  fi
+  if [ -d "/etc/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm82.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  fi
+fi
