dc99ed4192bc0986c4746553f7e07c504779d9c7
Pete Cooper <petecooper@users.noreply.github.com>
2022-07-05 13:46:37 +0100

    Update __php-version-check.sh
    idiot

--- __php-version-check.sh
+++ __php-version-check.sh
@@ -67,11 +67,11 @@ fi
 /etc/php/8.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
-  echo -e "\n8.2     php CLI: PHP "$(/etc/php/8.1/bin/php -r 'echo PHP_VERSION;')
-  echo -e "8.2 php-fpm CLI: PHP "$(/etc/php/8.1/sbin/php-fpm -i | grep "PHP Version" | head -1 | sed 's/.*=> //')
+  echo -e "\n8.2     php CLI: PHP "$(/etc/php/8.2/bin/php -r 'echo PHP_VERSION;')
+  echo -e "8.2 php-fpm CLI: PHP "$(/etc/php/8.2/sbin/php-fpm -i | grep "PHP Version" | head -1 | sed 's/.*=> //')
   echo -e "8.2 socket test: PHP" $(curl -s 'http://127.0.0.1:882/php-fpm-ver.php')
-  echo -e "8.2 php CLI SSL:" $(/etc/php/8.1/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
-  echo -e "8.2 php-fpm SSL:" $(/etc/php/8.1/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
+  echo -e "8.2 php CLI SSL:" $(/etc/php/8.2/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
+  echo -e "8.2 php-fpm SSL:" $(/etc/php/8.2/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
 
 fi
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
