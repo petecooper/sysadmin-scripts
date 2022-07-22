596a9c28a268e0c92b531b0914f0ac5b11a41ed1
Pete Cooper <pete@pragmatika.net>
2022-07-22 09:18:28 +0100

    zap older php versions from check

--- __php-version-check.sh
+++ __php-version-check.sh
@@ -4,38 +4,6 @@ if [ $? -eq 0 ]
 then
   echo -e "symlink: PHP "$(/usr/local/bin/php -r 'echo PHP_VERSION;')
 fi
-/etc/php/7.0/sbin/php-fpm -v &> /dev/null
-if [ $? -eq 0 ]
-then
-  echo -e "\n7.0     php CLI: PHP "$(/etc/php/7.0/bin/php -r 'echo PHP_VERSION;')
-  echo -e "7.0 php-fpm CLI: PHP "$(/etc/php/7.0/sbin/php-fpm -i | grep "PHP Version" | head -1 | sed 's/.*=> //')
-  echo -e "7.0 php CLI SSL:" $(/etc/php/7.0/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
-  echo -e "7.0 php-fpm SSL:" $(/etc/php/7.0/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
-fi
-/etc/php/7.1/sbin/php-fpm -v &> /dev/null
-if [ $? -eq 0 ]
-then
-  echo -e "\n7.1     php CLI: PHP "$(/etc/php/7.1/bin/php -r 'echo PHP_VERSION;')
-  echo -e "7.1 php-fpm CLI: PHP "$(/etc/php/7.1/sbin/php-fpm -i | grep "PHP Version" | head -1 | sed 's/.*=> //')
-  echo -e "7.1 php CLI SSL:" $(/etc/php/7.1/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
-  echo -e "7.1 php-fpm SSL:" $(/etc/php/7.1/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
-fi
-/etc/php/7.2/sbin/php-fpm -v &> /dev/null
-if [ $? -eq 0 ]
-then
-  echo -e "\n7.2     php CLI: PHP "$(/etc/php/7.2/bin/php -r 'echo PHP_VERSION;')
-  echo -e "7.2 php-fpm CLI: PHP "$(/etc/php/7.2/sbin/php-fpm -i | grep "PHP Version" | head -1 | sed 's/.*=> //')
-  echo -e "7.2 php CLI SSL:" $(/etc/php/7.2/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
-  echo -e "7.2 php-fpm SSL:" $(/etc/php/7.2/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
-fi
-/etc/php/7.3/sbin/php-fpm -v &> /dev/null
-if [ $? -eq 0 ]
-then
-  echo -e "\n7.3     php CLI: PHP "$(/etc/php/7.3/bin/php -r 'echo PHP_VERSION;')
-  echo -e "7.3 php-fpm CLI: PHP "$(/etc/php/7.3/sbin/php-fpm -i | grep "PHP Version" | head -1 | sed 's/.*=> //')
-  echo -e "7.3 php CLI SSL:" $(/etc/php/7.3/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
-  echo -e "7.3 php-fpm SSL:" $(/etc/php/7.3/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
-fi
 /etc/php/7.4/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
 then
