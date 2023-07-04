2edcce97931f978acc71bb8accf871380167ade6
Pete Cooper <petecooper@users.noreply.github.com>
2023-07-04 15:58:14 +0100

    Update __php-version-check.sh
    add php 8.3

--- __php-version-check.sh
+++ __php-version-check.sh
@@ -30,7 +30,6 @@ then
   echo -e "8.1 socket test: PHP" $(curl -s 'http://127.0.0.1:881/php-fpm-ver.php')
   echo -e "8.1 php CLI SSL:" $(/etc/php/8.1/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
   echo -e "8.1 php-fpm SSL:" $(/etc/php/8.1/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
-
 fi
 /etc/php/8.2/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
@@ -40,7 +39,15 @@ then
   echo -e "8.2 socket test: PHP" $(curl -s 'http://127.0.0.1:882/php-fpm-ver.php')
   echo -e "8.2 php CLI SSL:" $(/etc/php/8.2/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
   echo -e "8.2 php-fpm SSL:" $(/etc/php/8.2/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
-
+fi
+/etc/php/8.3/sbin/php-fpm -v &> /dev/null
+if [ $? -eq 0 ]
+then
+  echo -e "\n8.3     php CLI: PHP "$(/etc/php/8.3/bin/php -r 'echo PHP_VERSION;')
+  echo -e "8.3 php-fpm CLI: PHP "$(/etc/php/8.3/sbin/php-fpm -i | grep "PHP Version" | head -1 | sed 's/.*=> //')
+  echo -e "8.3 socket test: PHP" $(curl -s 'http://127.0.0.1:883/php-fpm-ver.php')
+  echo -e "8.3 php CLI SSL:" $(/etc/php/8.3/bin/php -r 'echo OPENSSL_VERSION_TEXT;')
+  echo -e "8.3 php-fpm SSL:" $(/etc/php/8.3/sbin/php-fpm -i | grep "OpenSSL Library Version" | sed 's/.*=> //')
 fi
 /etc/php/php-src/sbin/php-fpm -v &> /dev/null
 if [ $? -eq 0 ]
