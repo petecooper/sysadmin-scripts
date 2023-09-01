78e2c837939629c02d377e0d7b391c9b1cef55da
Pete Cooper <pete@pragmatika.net>
2023-09-01 17:26:12 +0100

    add php 8.3 socket check

--- __php-socket.sh
+++ __php-socket.sh
@@ -35,3 +35,12 @@ if [ -S "/var/run/php/php-fpm82.sock" ]; then
     egrep -lir "php-fpm82.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
   fi
 fi
+if [ -S "/var/run/php/php-fpm83.sock" ]; then
+  echo -e "\nFound PHP 8.3 socket at \`/var/run/php/php-fpm83.sock\`."
+  if [ -d "/etc/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm83.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  fi
+  if [ -d "/etc/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm83.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  fi
+fi
