6b5bcf07ca5432db36cacd96a0aff865ffc80472
Pete Cooper <petecooper@users.noreply.github.com>
2024-02-13 17:35:16 +0000

    Update __journal-vacuum.sh

--- __journal-vacuum.sh
+++ __journal-vacuum.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo journalctl --disk-usage \
-&& && read -p 'Journal vacuum size (e.g. 100M): ' journal_vacuum_size \
+&& read -p 'Journal vacuum size (e.g. 100M): ' journal_vacuum_size \
 && sudo journalctl --vacuum-size="$journal_vacuum_size"
