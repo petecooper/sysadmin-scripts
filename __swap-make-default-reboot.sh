7addb03585cee27618d0754d52382c2cac11f12e
Pete Cooper <petecooper@users.noreply.github.com>
2024-03-19 20:23:02 +0000

    Create __swap-make-default-reboot.sh

---
+++ __swap-make-default-reboot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if \
+[[ -f /etc/swap-file-size ]] \
+; then \
+sudo fallocate -l "$(< /etc/swap-file-size)" /swapfile \
+&& sudo chmod 600 /swapfile \
+&& sudo mkswap /swapfile \
+&& sudo swapon /swapfile --show \
+&& sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab' \
+&& echo -e '**********************************' \
+&& echo -e '* Swapfile created. Rebooting... *' \
+&& echo -e '**********************************' \
+&& sudo reboot \
+; else \
+echo '************************************' \
+&& echo '* `/etc/swap-file-size` not found. *' \
+&& echo '************************************' \
+; fi
