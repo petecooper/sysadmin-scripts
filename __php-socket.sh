c51a851824cac18a030b1383cc40e9341cee9230
Pete Cooper <pete@apropos.local>
2024-05-15 10:35:27 +0100

    tidy up

--- __php-socket.sh
+++ __php-socket.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 if [ -S "/var/run/php/php-fpm81.sock" ]; then
   echo -e "\nFound PHP 8.1 socket at \`/var/run/php/php-fpm81.sock\`."
-  if [ -d "/etc/nginx/servers-available/" ]; then
-    egrep -lir "php-fpm81.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm81.sock" /opt/nginx/servers-available/ | sed 's/^/-> /g' | sort
   fi
-  if [ -d "/etc/nginx/sites-available/" ]; then
-    egrep -lir "php-fpm81.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm81.sock" /opt/nginx/sites-available/ | sed 's/^/-> /g' | sort
   fi
 fi
 if [ -S "/var/run/php/php-fpm82.sock" ]; then
   echo -e "\nFound PHP 8.2 socket at \`/var/run/php/php-fpm82.sock\`."
-  if [ -d "/etc/nginx/servers-available/" ]; then
-    egrep -lir "php-fpm82.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm82.sock" /opt/nginx/servers-available/ | sed 's/^/-> /g' | sort
   fi
-  if [ -d "/etc/nginx/sites-available/" ]; then
-    egrep -lir "php-fpm82.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm82.sock" /opt/nginx/sites-available/ | sed 's/^/-> /g' | sort
   fi
 fi
 if [ -S "/var/run/php/php-fpm83.sock" ]; then
   echo -e "\nFound PHP 8.3 socket at \`/var/run/php/php-fpm83.sock\`."
-  if [ -d "/etc/nginx/servers-available/" ]; then
-    egrep -lir "php-fpm83.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm83.sock" /opt/nginx/servers-available/ | sed 's/^/-> /g' | sort
   fi
-  if [ -d "/etc/nginx/sites-available/" ]; then
-    egrep -lir "php-fpm83.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm83.sock" /opt/nginx/sites-available/ | sed 's/^/-> /g' | sort
   fi
 fi
 if [ -S "/var/run/php/php-fpm84.sock" ]; then
   echo -e "\nFound PHP 8.4 socket at \`/var/run/php/php-fpm84.sock\`."
-  if [ -d "/etc/nginx/servers-available/" ]; then
-    egrep -lir "php-fpm84.sock" /etc/nginx/servers-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/servers-available/" ]; then
+    egrep -lir "php-fpm84.sock" /opt/nginx/servers-available/ | sed 's/^/-> /g' | sort
   fi
-  if [ -d "/etc/nginx/sites-available/" ]; then
-    egrep -lir "php-fpm84.sock" /etc/nginx/sites-available/ | sed 's/^/-> /g' | sort
+  if [ -d "/opt/nginx/sites-available/" ]; then
+    egrep -lir "php-fpm84.sock" /opt/nginx/sites-available/ | sed 's/^/-> /g' | sort
   fi
 fi
