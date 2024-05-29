2f82cb3605f73c477d4543c21aa363f16f508995
Pete Cooper <pete@apropos.local>
2024-05-29 21:11:07 +0100

    first pass of nginx-vendor-version

--- __nginx-vendor-version.sh
+++ __nginx-vendor-version.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-if [ -f "/etc/nginx/nginx.conf" ]; then
-  echo "Found conf at /etc/nginx/"
-fi
-if [ -f "/opt/nginx/nginx.conf" ]; then
-  echo "Found conf at /opt/nginx/"
-fi
-if [ -f "/opt/freenginx/nginx/nginx.conf" ]; then
-  echo "Found conf at /opt/freenginx/nginx/"
-fi
-if [ -f "/opt/nginx/nginx/nginx.conf" ]; then
-  echo "Found conf at /opt/nginx/nginx/"
-fi
+sudo /usr/sbin/nginx -v 2>&1 | tee -a ~/nginx-v > /dev/null \
+&& cp ~/nginx-v ~/nginx-vendor \
+&& cp ~/nginx-v ~/nginx-version \
+&& sed -i -e "s/nginx version: //" ~/nginx-vendor \
+&& sed -i -e "s/nginx.*/nginx/" ~/nginx-vendor \
+&& sed -i -e "s/^[^0-9]*//" ~/nginx-version \
+&& sed -i -e "s/[[:space:]].*//" ~/nginx-version \
+&& sudo mv ~/nginx-v /etc/nginx-v \
+&& sudo mv ~/nginx-vendor /etc/nginx-vendor \
+&& sudo mv ~/nginx-version /etc/nginx-version \
+&& sudo chown root:sudo \
+/etc/nginx-v \
+/etc/nginx-vendor \
+/etc/nginx-version \
+&& sudo chmod 775 \
+/etc/nginx-v \
+/etc/nginx-vendor \
+/etc/nginx-version
