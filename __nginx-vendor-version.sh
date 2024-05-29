e16e4c9755b748318a54d748dac382d8f3295b89
Pete Cooper <pete@apropos.local>
2024-05-29 20:30:41 +0100

    start nginx-vendor-version

--- __nginx-vendor-version.sh
+++ __nginx-vendor-version.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+if [ -f "/etc/nginx/nginx.conf" ]; then
+  echo "Found conf at /etc/nginx/"
+fi
+if [ -f "/opt/nginx/nginx.conf" ]; then
+  echo "Found conf at /opt/nginx/"
+fi
+if [ -f "/opt/freenginx/nginx/nginx.conf" ]; then
+  echo "Found conf at /opt/freenginx/nginx/"
+fi
+if [ -f "/opt/nginx/nginx/nginx.conf" ]; then
+  echo "Found conf at /opt/nginx/nginx/"
+fi
