531346f367581949ff457032a9702d8ac3403716
Pete Cooper <petecooper@users.noreply.github.com>
2024-08-15 13:58:34 +0100

    Update __composer-install-local.sh

--- __composer-install-local.sh
+++ __composer-install-local.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 then
-    >&2 echo 'ERROR: Invalid installer signature'
+    >&2 echo 'ERROR: Invalid installer checksum'
     rm composer-setup.php
     exit 1
 fi
