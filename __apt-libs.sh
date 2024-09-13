9c1c004ef9b0bac32a208dbf6cc3bfc002075f6b
Pete Cooper <petecooper@users.noreply.github.com>
2024-09-13 20:01:39 +0100

    Update __apt-libs.sh

--- __apt-libs.sh
+++ __apt-libs.sh
@@ -3,13 +3,16 @@ sudo apt update \
 && sudo apt -y install \
 apt-transport-https \
 autoconf \
+autoconf-archive \
 automake \
+autotools-dev \
 bison \
 build-essential \
 ccache \
 certbot \
 clang \
 cmake \
+cpio
 dpkg-dev \
 fio \
 flent \
@@ -17,6 +20,7 @@ flex \
 g++ \
 gcc \
 gosu \
+graphviz \
 htop \
 libaio1 \
 libargon2-dev \
@@ -28,6 +32,7 @@ libbsd-dev \
 libbz2-dev \
 libcap-ng-dev \
 libcurl4-openssl-dev \
+libedit-dev \
 libelf-dev \
 libevent-dev \
 libfl-dev \
@@ -35,12 +40,14 @@ libgd-dev \
 libgeoip-dev \
 libhiredis-dev \
 libjansson-dev \
-libjson-c-dev \
+libjemalloc-dev \
 libjpeg-dev \
+libjson-c-dev \
 liblua5.4-dev \
 liblz4-dev \
 libmagic-dev \
 libmnl-dev \
+libncurses-dev \
 libncurses5-dev \
 libnet1-dev \
 libonig-dev \
@@ -66,11 +73,14 @@ mc \
 moreutils \
 multitail \
 ncdu \
+net-tools \
 ninja-build \
 pkg-config \
 python3-dev \
+python3-docutils \
 python3-git \
 python3-semantic-version \
+python3-sphinx \
 re2c \
 redis \
 rsync \
