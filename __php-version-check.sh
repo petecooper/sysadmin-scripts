1c05311808b224d991d59126a06ac109824f1327
Pete Cooper <pete@pragmatika.net>
2024-09-15 17:34:03 +0100

    ~

--- __php-version-check.sh
+++ __php-version-check.sh
@@ -9,8 +9,38 @@ echo -e '\n=> `php` symlink NOT set.' \
 && if \
 [[ -f /etc/php-prefix ]] \
 ; then \
-echo '\n=> `/etc/php-prefix` found.' \
+echo -e '\n=> `/etc/php-prefix` found.' \
 && php_prefix="$(< /etc/php-prefix)" \
 ; else \
-echo '\n=> `/etc/php-prefix` NOT found.' \
+echo -e '\n=> `/etc/php-prefix` NOT found.' \
+; fi \
+&& echo 'PHP 8.4 checks...' \
+&& if \
+[ -d "$php_prefix"/php/8.4/ ] || [ -d "$php_prefix"/php/8.4/nts/ ]|| [ -d "$php_prefix"/php/8.4/zts/ ] \
+; then \
+echo 'PHP 8.4 found.' \
+; fi \
+&& echo 'PHP 8.3 checks...' \
+&& if \
+[ -d "$php_prefix"/php/8.3/ ] || [ -d "$php_prefix"/php/8.3/nts/ ]|| [ -d "$php_prefix"/php/8.4/zts/ ] \
+; then \
+echo 'PHP 8.3 found.' \
+; fi \
+&& echo 'PHP 8.2 checks...' \
+&& if \
+[ -d "$php_prefix"/php/8.2/ ] || [ -d "$php_prefix"/php/8.4/nts/ ]|| [ -d "$php_prefix"/php/8.4/zts/ ] \
+; then \
+echo 'PHP 8.2 found.' \
+; fi \
+&& echo 'PHP 8.1 checks...' \
+&& if \
+[ -d "$php_prefix"/php/8.1/ ] || [ -d "$php_prefix"/php/8.4/nts/ ]|| [ -d "$php_prefix"/php/8.4/zts/ ] \
+; then \
+echo 'PHP 8.2 found.' \
+; fi \
+&& echo 'PHP src checks...' \
+&& if \
+[ -d "$php_prefix"/php/php-src/ ] || [ -d "$php_prefix"/php/php-src/nts/ ]|| [ -d "$php_prefix"/php/php-src/zts/ ] \
+; then \
+echo 'PHP src found.' \
 ; fi
