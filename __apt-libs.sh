8ee6c9bb58a315d93bb0172a9a4f6e71e194c3dd
Pete Cooper <petecooper@users.noreply.github.com>
2024-12-16 17:12:35 +0000

    Update __apt-libs.sh

--- __apt-libs.sh
+++ __apt-libs.sh
@@ -7,12 +7,15 @@ autoconf-archive \
 automake \
 autotools-dev \
 bison \
+bpftool \
 build-essential \
 ccache \
 certbot \
 clang \
 cmake \
 cpio \
+dialog \
+dkms \
 dpkg-dev \
 fio \
 flent \
@@ -22,6 +25,8 @@ gcc \
 gosu \
 graphviz \
 htop \
+lbzip2 \
+libaio-dev \
 libaio1 \
 libargon2-dev \
 libatomic-ops-dev \
@@ -30,22 +35,30 @@ libbpfcc-dev \
 libbrotli-dev \
 libbsd-dev \
 libbz2-dev \
+libcap-dev \
 libcap-ng-dev \
 libcurl4-openssl-dev \
 libedit-dev \
 libelf-dev \
+libev-dev \
 libevent-dev \
+libexpat-dev \
 libfl-dev \
+libfontconfig1 \
+libgcrypt-dev \
 libgd-dev \
 libgeoip-dev \
-libhiredis-dev \
 libjansson-dev \
 libjemalloc-dev \
 libjpeg-dev \
 libjson-c-dev \
+libkrb5-dev \
+libldap-dev \
 liblz4-dev \
 libmagic-dev \
 libmaxminddb-dev \
+libmemcached-dev \
+libmemcached-tools \
 libmnl-dev \
 libncurses-dev \
 libncurses5-dev \
@@ -55,6 +68,7 @@ libpcap-dev \
 libpcre2-dev \
 libpcre3-dev \
 libpng-dev \
+libproc2-dev \
 libprotobuf-dev \
 libsasl2-dev \
 libsodium-dev \
@@ -68,15 +82,21 @@ libxslt1-dev \
 libyaml-dev \
 libzip-dev \
 libzstd-dev \
+linux-headers-"$(uname -r)" \
+llvm \
 lrzip \
 lz4 \
 make \
 mc \
 moreutils \
 multitail \
+musl \
 ncdu \
 net-tools \
 ninja-build \
+patch \
+pbzip2 \
+pigz \
 pkg-config \
 python3-dev \
 python3-docutils \
@@ -84,14 +104,15 @@ python3-git \
 python3-semantic-version \
 python3-sphinx \
 re2c \
-redis \
 rsync \
 screen \
 swig \
 tuned \
+unzip \
 uuid-dev \
 whois \
 wireguard \
+xxd \
 zip \
 zlib1g-dev \
 zstd \
@@ -119,4 +140,4 @@ debian-goodies \
 && sudo apt -y clean \
 && sudo apt -y autoclean \
 && sudo apt -y autoremove --purge \
-&& dpkg -l linux-{image,headers}-* | awk '/^ii/{print $2}' | grep -E '[0-9]+\.[0-9]+\.[0-9]+' | grep -v "$(uname -r | cut -d- -f-2)" | xargs sudo apt -y purge \&& echo 'Done.'
+&& dpkg -l linux-{image,headers}-* | awk '/^ii/{print $2}' | grep -E '[0-9]+\.[0-9]+\.[0-9]+' | grep -v "$(uname -r | cut -d- -f-2)" | xargs sudo apt -y purge
