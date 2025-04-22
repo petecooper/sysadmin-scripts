5066e52c9d81e11ce8dafd140739c5924251186a
Pete Cooper <pete@pragmatika.net>
2025-04-22 14:16:17 +0100

    namespace

--- __tar-gz-var-www-nginx-var-log-nginx.sh
+++ __tar-gz-var-www-nginx-var-log-nginx.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar cvf - -C "$HOME" /var/www/ $(cat /etc/nginx-prefix)/nginx/ /var/log/nginx/ | gzip -c9 > $(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www_nginx_var-log-nginx.zip
+tar cvf - -C "$HOME" /var/www/ $(cat /etc/nginx-nginx-prefix)/nginx/ /var/log/nginx/ | gzip -c9 > $(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www_nginx_var-log-nginx.zip
