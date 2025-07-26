5d7a0c63d82f56d4d466788dfb3b319a96fbb2b2
Pete Cooper <pete@pragmatika.net>
2025-07-26 20:05:55 +0100

    tidy

--- __swap-status.sh
+++ __swap-status.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-if free | \
-awk '/^Swap:/ {exit !$2}'; then
-sudo swapon -show
-else
-echo 'No swap.'
-fi
+if \
+free | \
+awk '/^Swap:/ {exit !$2}' \
+; then \
+sudo swapon -show \
+; else \
+echo 'No swap.' \
+; fi
