c770c225cad323f04b4cc3ea93951860adef7e74
Pete Cooper <pete@pragmatika.net>
2025-07-26 20:17:51 +0100

    tidy

--- __nginx-prefix-namespace-fix.sh
+++ __nginx-prefix-namespace-fix.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 if [ -f "/etc/nginx-log-dir-base" ] ; then
-sudo mv /etc/nginx-log-dir-base /etc/nginx-nginx-log-dir-base
+sudo mv \
+/etc/nginx-log-dir-base \
+/etc/nginx-nginx-log-dir-base
 fi
 if [ -f "/etc/nginx-prefix" ] ; then
-sudo mv /etc/nginx-prefix /etc/nginx-nginx-prefix
+sudo mv \
+/etc/nginx-prefix \
+/etc/nginx-nginx-prefix
 fi
 if [ -f "/etc/nginx-tls-vendor" ] ; then
-sudo mv /etc/nginx-tls-vendor /etc/nginx-nginx-tls-vendor
+sudo mv \
+/etc/nginx-tls-vendor \
+/etc/nginx-nginx-tls-vendor
 fi
