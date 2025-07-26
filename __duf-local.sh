d1bc63fc14a11f6cdba4cb78623e64a6015f3114
Pete Cooper <pete@pragmatika.net>
2025-07-26 18:27:34 +0100

    tidy

--- __duf-local.sh
+++ __duf-local.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-if [ -f "/usr/bin/duf" ] ; then
-    /usr/bin/duf --only local --only-fs ext4,xfs --output mountpoint,size,used,avail,usage --width 115
-fi
+if \
+[ -f "/usr/bin/duf" ] \
+; then
+/usr/bin/duf \
+--only local \
+--only-fs ext4,xfs \
+--output mountpoint,size,used,avail,usage \
+--width 115 \
+; fi
--- __find-big-files-slash.sh
+++ __find-big-files-slash.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-find / -xdev -type f -size +20M -exec du -sh {} ';' | \
+find / \
+-xdev \
+-type f \
+-size +20M \
+-exec du -sh {} ';' | \
 sort -rh | \
 head -n50
