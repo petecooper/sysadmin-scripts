96c2ffa39e30f7aaea68957c21f1ff62892bea5a
Pete Cooper <pete@pragmatika.net>
2025-07-26 18:28:29 +0100

    tidy

--- __journal-vacuum.sh
+++ __journal-vacuum.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo journalctl --disk-usage \
+sudo journalctl \
+--disk-usage \
 && read -p 'Journal vacuum size (e.g. 100M): ' journal_vacuum_size \
-&& sudo journalctl --vacuum-size="$journal_vacuum_size"
+&& sudo journalctl \
+--vacuum-size="$journal_vacuum_size"
