ef3651eb68ebe642513fc782ce0f99169bc81cb1
Pete Cooper <petecooper@users.noreply.github.com>
2025-09-01 10:47:21 +0100

    Update __zip-var-www-nginx-var-log-nginx.sh

--- __zip-var-www-nginx-var-log-nginx.sh
+++ __zip-var-www-nginx-var-log-nginx.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo zip --symlinks -r "$HOME"/"$(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www_etc-nginx_var-log-nginx.zip" /var/www/ $(cat /etc/nginx-nginx-prefix)/nginx/ /var/log/nginx/
+sudo zip -9 --symlinks -r "$HOME"/"$(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www_etc-nginx_var-log-nginx.zip" /var/www/ $(cat /etc/nginx-nginx-prefix)/nginx/ /var/log/nginx/
