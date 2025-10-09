b85f968c2bef785f60fcca44beec84afb0b40069
Pete Cooper <petecooper@users.noreply.github.com>
2025-10-09 10:41:16 +0100

    Update __apt-libs.sh

--- __apt-libs.sh
+++ __apt-libs.sh
@@ -184,4 +184,5 @@ awk '/^ii/{print $2}' | \
 grep -E '[0-9]+\.[0-9]+\.[0-9]+' | \
 grep -v "$(uname -r | \
 cut -d- -f-2)" | \
-xargs sudo apt -y purge \&& echo 'Done.'
+xargs sudo apt -y purge \
+&& echo 'Done.'
