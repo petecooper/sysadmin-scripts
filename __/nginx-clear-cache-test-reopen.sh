a484f4deab73b236943dff3a30d71823268fdec0
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:36 +0000

    ~

--- __/nginx-clear-cache-test-reopen.sh
+++ __/nginx-clear-cache-test-reopen.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-if [ -f "/usr/sbin/nginx" ] ; then
-sudo /usr/sbin/nginx -t \
-&& sudo rm -rf /var/cache/nginx/* \
-&& sudo /usr/sbin/nginx -s reopen
-fi
-if [ -f "/usr/sbin/nginx/nginx" ] ; then
-sudo /usr/sbin/nginx/nginx -t \
-&& sudo rm -rf /var/cache/nginx/nginx/* \
-&& sudo /usr/sbin/nginx/nginx -s reopen
-fi
+if \
+    [ -f "/usr/sbin/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx -t \
+    && sudo rm -rf /var/cache/nginx/* \
+    && sudo /usr/sbin/nginx -s reopen \
+; fi \
+&& if \
+        [ -f "/usr/sbin/nginx/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx/nginx -t \
+    && sudo rm -rf /var/cache/nginx/nginx/* \
+    && sudo /usr/sbin/nginx/nginx -s reopen \
+; fi
