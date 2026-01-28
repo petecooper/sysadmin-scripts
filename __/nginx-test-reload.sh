a9bb0b65f3b4c35be0e33786ada55cf054205cbd
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:52 +0000

    ~

--- __/nginx-test-reload.sh
+++ __/nginx-test-reload.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-if [ -f "/usr/sbin/nginx" ] ; then
-sudo /usr/sbin/nginx -t \
-&& sudo /usr/sbin/nginx -s reload
-fi
-if [ -f "/usr/sbin/nginx/nginx" ] ; then
-sudo /usr/sbin/nginx/nginx -t \
-&& sudo /usr/sbin/nginx/nginx -s reload
-fi
+if \
+    [ -f "/usr/sbin/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx -t \
+    && sudo /usr/sbin/nginx -s reload \
+; fi \
+&& if \
+    [ -f "/usr/sbin/nginx/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx/nginx -t \
+    && sudo /usr/sbin/nginx/nginx -s reload \
+; fi
