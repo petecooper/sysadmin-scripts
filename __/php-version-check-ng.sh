ebec86f7f024044c748e23be0317a3afb0bf4d4c
Pete Cooper <pete@pragmatika.net>
2026-01-28 14:20:48 +0000

    ~

--- __/php-version-check-ng.sh
+++ __/php-version-check-ng.sh
@@ -13,7 +13,8 @@ if \
             && if \
                 [ -d "$php_prefix"/php/8.5/nts ] \
             ; then \
-                if \
+                echo -e '=>    PHP: 8.5 NTS' \
+                && if \
                     [ -f "$php_prefix"/php/8.5/nts/bin/php ] \
                 ; then \
                     unset \
@@ -53,6 +54,59 @@ if \
                     ; fi \
                     && if \
                         [ -S /run/php/php/8.5/nts/php-php-fpm85nts.sock ] \
+                    ; then \
+                        echo -e '`php-fpm`: .sock found.' \
+                    ; else \
+                        echo -e '`php-fpm`: .sock NOT found.' \
+                    ; fi \
+                ; else \
+                    echo -e '=> PHP 8.5 NTS scaffold found; `php-fpm` NOT found.' \
+                ; fi \
+            ; fi \
+            && if \
+                [ -d "$php_prefix"/php/8.5/zts ] \
+            ; then \
+                echo -e '=>    PHP: 8.5 ZTS' \
+                && if \
+                    [ -f "$php_prefix"/php/8.5/zts/bin/php ] \
+                ; then \
+                    unset \
+                    php_thread_safety_bool \
+                    php_thread_safety_status_upper \
+                    && php_thread_safety_bool="$("$php_prefix"/php/8.5/zts/bin/php -r 'echo ZEND_THREAD_SAFE;')" \
+                    && if \
+                        [ "$php_thread_safety_bool" = "1" ] \
+                    ; then \
+                        php_thread_safety_status_upper="ZTS" \
+                    ; else \
+                        php_thread_safety_status_upper="NTS" \
+                    ; fi \
+                    && echo -e '    `php`:' \
+                    "$("$php_prefix"/php/8.5/zts/bin/php -r 'echo PHP_VERSION;')" \
+                    '('"$php_thread_safety_status_upper"'; '"$("$php_prefix"/php/8.5/zts/bin/php -r 'echo OPENSSL_VERSION_TEXT;')"')' \
+                ; else \
+                    echo -e '=> PHP 8.5 NTS scaffold found; `php` NOT found.' \
+                ; fi \
+                && if \
+                    [ -f "$php_prefix"/php/8.5/zts/sbin/php-fpm ] \
+                ; then \
+                    echo -e '`php-fpm`:' \
+                    "$("$php_prefix"/php/8.5/zts/sbin/php-fpm -i | \
+                    grep "PHP Version" | \
+                    head -1 | \
+                    sed 's/.*=> //')" \
+                    '     ('"$("$php_prefix"/php/8.5/zts/sbin/php-fpm -i | \
+                    grep "OpenSSL Library Version" | \
+                    sed 's/.*=> //')"')' \
+                    && if \
+                        [ -f /run/php/php/8.5/zts/php-php-fpm85nts.pid ] \
+                    ; then \
+                        echo -e '`php-fpm`: .pid found.' \
+                    ; else \
+                        echo -e '`php-fpm`: .pid NOT found.' \
+                    ; fi \
+                    && if \
+                        [ -S /run/php/php/8.5/zts/php-php-fpm85nts.sock ] \
                     ; then \
                         echo -e '`php-fpm`: .sock found.' \
                     ; else \
