9c078151367dce8d1f7b62d151094a6576af76ab
Pete Cooper <pete@pragmatika.net>
2026-01-28 13:38:45 +0000

    ~

--- __/php-version-check-ng.sh
+++ __/php-version-check-ng.sh
@@ -33,10 +33,7 @@ if \
                     ; fi \
                     && echo -e '    `php`:' \
                     "$("$php_prefix"/php/8.5/nts/bin/php -r 'echo PHP_VERSION;')" \
-                    '(' \
-                    "$php_thread_safety_status_upper" \
-                    "$("$php_prefix"/php/8.5/nts/bin/php -r 'echo OPENSSL_VERSION_TEXT;')" \
-                    ')' \
+                    '('"$php_thread_safety_status_upper"';'"$("$php_prefix"/php/8.5/nts/bin/php -r 'echo OPENSSL_VERSION_TEXT;')"')' \
                 ; else \
                     echo -e '=> PHP 8.5 NTS `php` NOT found.' \
                 ; fi \
