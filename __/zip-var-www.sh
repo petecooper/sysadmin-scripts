179dca7730d61bb5d773d39534ab00436acbc3c9
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:03:55 +0000

    ~

--- __/zip-var-www.sh
+++ __/zip-var-www.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo zip -9 --symlinks -r ~/"$(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www.zip" /var/www/
+zip -9 \
+--symlinks \
+-r \
+"$HOME"/"$(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www.zip" \
+/var/www/
