968e6778ca2ea96437ae2d514a09cfce38004e18
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:03:03 +0000

    ~

--- __/snapshot-restore-first-boot-default-swap.sh
+++ __/snapshot-restore-first-boot-default-swap.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 if \
-[[ -s /etc/swap-file-size ]] \
+    [[ -s /etc/swap-file-size ]] \
 ; then \
-swap_size="$(< /etc/swap-file-size)" \
-&& sudo fallocate -l "$swap_size" /swapfile \
-&& sudo chmod 600 /swapfile \
-&& sudo mkswap /swapfile \
-&& sudo swapon /swapfile --show \
-&& sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab' \
-&& echo -e '\n=> Swapfile size: '"$swap_size" \
-&& echo -e '\n*********************' \
-&& echo -e '* Swapfile created. *' \
-&& echo -e '*********************' \
-&& sudo apt update \
-&& sudo apt -y dist-upgrade \
-&& sudo apt -y autoclean \
-&& sudo apt -y clean \
-&& sudo apt -y autoremove --purge \
-&& echo -e '\n********************' \
-&& echo -e '* Done. Rebooting. *' \
-&& echo -e '********************' \
-&& sudo reboot \
+    swap_size="$(< /etc/swap-file-size)" \
+    && sudo fallocate -l "$swap_size" /swapfile \
+    && sudo chmod 600 /swapfile \
+    && sudo mkswap /swapfile \
+    && sudo swapon /swapfile --show \
+    && sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab' \
+    && echo -e '\n=> Swapfile size: '"$swap_size" \
+    && echo -e '\n*********************' \
+    && echo -e '* Swapfile created. *' \
+    && echo -e '*********************' \
+    && sudo apt update \
+    && sudo apt -y dist-upgrade \
+    && sudo apt -y autoclean \
+    && sudo apt -y clean \
+    && sudo apt -y autoremove --purge \
+    && echo -e '\n********************' \
+    && echo -e '* Done. Rebooting. *' \
+    && echo -e '********************' \
+    && sudo reboot \
 ; else \
-echo -e '\n************************************' \
-&& echo '* `/etc/swap-file-size` not found. *' \
-&& echo '************************************' \
+    echo -e '\n************************************' \
+    && echo '* `/etc/swap-file-size` not found. *' \
+    && echo '************************************' \
 ; fi
