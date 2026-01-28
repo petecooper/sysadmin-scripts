4a5e05c1c4a70c2ac51dba09cd7b5fe0874f10ac
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:03:51 +0000

    ~

--- __/zip-var-www-nginx.sh
+++ __/zip-var-www-nginx.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo zip -9 --symlinks -r "$HOME"/"$(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www_etc-nginx.zip" /var/www/ $(cat /etc/nginx-nginx-prefix)/nginx/
+zip -9 \
+--symlinks \
+-r \
+"$HOME"/"$(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www_nginx.zip" \
+/var/www/ \
+"$(cat /etc/nginx-nginx-prefix)"/nginx/
