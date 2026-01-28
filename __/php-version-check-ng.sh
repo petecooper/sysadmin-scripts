f3b09195c7c8c645caa7c7104a6dffa8a1a1182f
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:10:25 +0000

    ~

--- __/php-version-check-ng.sh
+++ __/php-version-check-ng.sh
@@ -357,6 +357,123 @@ if \
                 ; fi \
             ; fi \
         ; fi \
+        if \
+            [ -d "$php_prefix"/php/8.2/ ] \
+        ; then \
+            echo -e '=>    PHP: 8.2' \
+            && if \
+                [ -d "$php_prefix"/php/8.2/nts ] \
+            ; then \
+                echo -e '=>    PHP: 8.2 NTS' \
+                && if \
+                    [ -f "$php_prefix"/php/8.2/nts/bin/php ] \
+                ; then \
+                    unset \
+                    php_thread_safety_bool \
+                    php_thread_safety_status_upper \
+                    && php_thread_safety_bool="$("$php_prefix"/php/8.2/nts/bin/php -r 'echo ZEND_THREAD_SAFE;')" \
+                    && if \
+                        [ "$php_thread_safety_bool" = "1" ] \
+                    ; then \
+                        php_thread_safety_status_upper="ZTS" \
+                    ; else \
+                        php_thread_safety_status_upper="NTS" \
+                    ; fi \
+                    && echo -e '    `php`:' \
+                    "$("$php_prefix"/php/8.2/nts/bin/php -r 'echo PHP_VERSION;')" \
+                    '('"$php_thread_safety_status_upper"'; '"$("$php_prefix"/php/8.2/nts/bin/php -r 'echo OPENSSL_VERSION_TEXT;')"')' \
+                    && unset \
+                    php_thread_safety_bool \
+                    php_thread_safety_status_upper \
+                ; else \
+                    echo -e '=> PHP 8.2 NTS scaffold found; `php` NOT found.' \
+                ; fi \
+                && if \
+                    [ -f "$php_prefix"/php/8.2/nts/sbin/php-fpm ] \
+                ; then \
+                    echo -e '`php-fpm`:' \
+                    "$("$php_prefix"/php/8.2/nts/sbin/php-fpm -i | \
+                    grep "PHP Version" | \
+                    head -1 | \
+                    sed 's/.*=> //')" \
+                    '     ('"$("$php_prefix"/php/8.2/nts/sbin/php-fpm -i | \
+                    grep "OpenSSL Library Version" | \
+                    sed 's/.*=> //')"')' \
+                    && if \
+                        [ -f /run/php/php/8.2/nts/php-php-fpm82nts.pid ] \
+                    ; then \
+                        echo -e '`php-fpm`: .pid found.' \
+                    ; else \
+                        echo -e '`php-fpm`: .pid NOT found.' \
+                    ; fi \
+                    && if \
+                        [ -S /run/php/php/8.2/nts/php-php-fpm82nts.sock ] \
+                    ; then \
+                        echo -e '`php-fpm`: .sock found.' \
+                    ; else \
+                        echo -e '`php-fpm`: .sock NOT found.' \
+                    ; fi \
+                ; else \
+                    echo -e '=> PHP 8.2 NTS scaffold found; `php-fpm` NOT found.' \
+                ; fi \
+            ; fi \
+            && if \
+                [ -d "$php_prefix"/php/8.2/zts ] \
+            ; then \
+                echo -e '=>    PHP: 8.2 ZTS' \
+                && if \
+                    [ -f "$php_prefix"/php/8.2/zts/bin/php ] \
+                ; then \
+                    unset \
+                    php_thread_safety_bool \
+                    php_thread_safety_status_upper \
+                    && php_thread_safety_bool="$("$php_prefix"/php/8.2/zts/bin/php -r 'echo ZEND_THREAD_SAFE;')" \
+                    && if \
+                        [ "$php_thread_safety_bool" = "1" ] \
+                    ; then \
+                        php_thread_safety_status_upper="ZTS" \
+                    ; else \
+                        php_thread_safety_status_upper="NTS" \
+                    ; fi \
+                    && echo -e '    `php`:' \
+                    "$("$php_prefix"/php/8.2/zts/bin/php -r 'echo PHP_VERSION;')" \
+                    '('"$php_thread_safety_status_upper"'; '"$("$php_prefix"/php/8.2/zts/bin/php -r 'echo OPENSSL_VERSION_TEXT;')"')' \
+                    && unset \
+                    php_thread_safety_bool \
+                    php_thread_safety_status_upper \
+                ; else \
+                    echo -e '=> PHP 8.2 NTS scaffold found; `php` NOT found.' \
+                ; fi \
+                && if \
+                    [ -f "$php_prefix"/php/8.2/zts/sbin/php-fpm ] \
+                ; then \
+                    echo -e '`php-fpm`:' \
+                    "$("$php_prefix"/php/8.2/zts/sbin/php-fpm -i | \
+                    grep "PHP Version" | \
+                    head -1 | \
+                    sed 's/.*=> //')" \
+                    '     ('"$("$php_prefix"/php/8.2/zts/sbin/php-fpm -i | \
+                    grep "OpenSSL Library Version" | \
+                    sed 's/.*=> //')"')' \
+                    && if \
+                        [ -f /run/php/php/8.2/zts/php-php-fpm82nts.pid ] \
+                    ; then \
+                        echo -e '`php-fpm`: .pid found.' \
+                    ; else \
+                        echo -e '`php-fpm`: .pid NOT found.' \
+                    ; fi \
+                    && if \
+                        [ -S /run/php/php/8.2/zts/php-php-fpm82nts.sock ] \
+                    ; then \
+                        echo -e '`php-fpm`: .sock found.' \
+                    ; else \
+                        echo -e '`php-fpm`: .sock NOT found.' \
+                    ; fi \
+                ; else \
+                    echo -e '=> PHP 8.2 NTS scaffold found; `php-fpm` NOT found.' \
+                ; fi \
+            ; fi \
+        ; fi \
     ; else \
         echo -e '=> PHP prefix dir NOT found.' \
     ; fi \
