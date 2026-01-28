d0cd4ffd3e14d8910273e992fb7684a6b577b353
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:20 +0000

    ~

--- __/duf-local.sh
+++ __/duf-local.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if \
-[ -f "/usr/bin/duf" ] \
+    [ -f "/usr/bin/duf" ] \
 ; then
-/usr/bin/duf \
---only local \
---only-fs ext4,xfs \
---output mountpoint,size,used,avail,usage \
---width 115 \
+    /usr/bin/duf \
+    --only local \
+    --only-fs ext4,xfs \
+    --output mountpoint,size,used,avail,usage \
+    --width 115 \
 ; fi
