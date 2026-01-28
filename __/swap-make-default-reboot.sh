cfdefcae8a1e1e3ce904b02636b779af1d11a609
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:03:08 +0000

    ~

--- __/swap-make-default-reboot.sh
+++ __/swap-make-default-reboot.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 if \
-[[ -s /etc/swap-file-size ]] \
+    [[ -s /etc/swap-file-size ]] \
 ; then \
-swap_size="$(< /etc/swap-file-size)" \
-&& sudo fallocate -l "$swap_size" /swapfile \
-&& sudo chmod 600 /swapfile \
-&& sudo mkswap /swapfile \
-&& sudo swapon /swapfile --show \
-&& sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab' \
-&& echo -e '\n=> Swapfile size: '"$swap_size" \
-&& echo -e '\n**********************************' \
-&& echo -e '* Swapfile created. Rebooting... *' \
-&& echo -e '**********************************' \
-&& sudo reboot \
+    swap_size="$(< /etc/swap-file-size)" \
+    && sudo fallocate -l "$swap_size" /swapfile \
+    && sudo chmod 600 /swapfile \
+    && sudo mkswap /swapfile \
+    && sudo swapon /swapfile --show \
+    && sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab' \
+    && echo -e '\n=> Swapfile size: '"$swap_size" \
+    && echo -e '\n**********************************' \
+    && echo -e '* Swapfile created. Rebooting... *' \
+    && echo -e '**********************************' \
+    && sudo reboot \
 ; else \
-echo -e '\n************************************' \
-&& echo '* `/etc/swap-file-size` not found. *' \
-&& echo '************************************' \
+    echo -e '\n************************************' \
+    && echo '* `/etc/swap-file-size` not found. *' \
+    && echo '************************************' \
 ; fi
