94a86774ffe72d28208d49da5daa458eb953062e
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:00:40 +0000

    ~

--- __/apt-upgrade-clean-deep-shutdown-dbg.sh
+++ __/apt-upgrade-clean-deep-shutdown-dbg.sh
@@ -7,7 +7,7 @@ sudo apt -oDebug::pkgAcquire::Worker=1 update \
 && dpkg -l linux-{image,headers}-* | \
 awk '/^ii/{print $2}' | \
 grep -E '[0-9]+\.[0-9]+\.[0-9]+' | \
-grep -v $(uname -r | \
-cut -d- -f-2) | \
+grep -v "$(uname -r | \
+cut -d- -f-2)" | \
 xargs sudo apt -oDebug::pkgAcquire::Worker=1 -y purge \
 && sudo shutdown -h now
