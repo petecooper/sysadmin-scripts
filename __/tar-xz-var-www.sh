d9b42ce37cf60632952e46982dba20f08a2e2285
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:03:40 +0000

    ~

--- __/tar-xz-var-www.sh
+++ __/tar-xz-var-www.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-tar cvf - -C "$HOME" /var/www/ | \
-xz -9e -z -q > $(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www.zip
+tar cvf - \
+-C "$HOME" \
+/var/www/ \
+xz -9e -z -q > \
+"$(date +"%Y%m%d_%H%M%S")"-"$(hostname)"-var-www.tar.xz
