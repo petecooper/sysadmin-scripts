695d4e14b9b52933c13d49a344f777c04cf7c84b
Pete Cooper <pete@pragmatika.net>
2026-01-28 12:46:31 +0000

    ~

--- __/php-version-check-ng.sh
+++ __/php-version-check-ng.sh
@@ -3,6 +3,7 @@ if \
 [[ -f /etc/php-php-prefix ]] \
 ; then \
     php_prefix="$(< /etc/php-php-prefix)" \
+    && echo -e '`/etc/php-php-prefix` found.' \
 ; else \
     echo -e '`/etc/php-php-prefix` NOT found.' \
 ; fi
