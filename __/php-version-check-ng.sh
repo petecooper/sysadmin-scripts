e461fe7997c4de654857f3ac67127356ae253593
Pete Cooper <pete@pragmatika.net>
2026-01-28 12:51:20 +0000

    ~

--- __/php-version-check-ng.sh
+++ __/php-version-check-ng.sh
@@ -3,7 +3,22 @@ if \
 [[ -f /etc/php-php-prefix ]] \
 ; then \
     php_prefix="$(< /etc/php-php-prefix)" \
-    && echo -e '`/etc/php-php-prefix` found.' \
+    && echo -e '=> `/etc/php-php-prefix` found.' \
+    && if \
+    [ -d "$php_prefix"/php/8.5/ ] \
+    ; then \
+        echo -e '=> PHP 8.5 scaffold found.' \
+        && if \
+            [ -d "$php_prefix"/php/8.5/nts/ ] \
+            ; then \
+                echo -e '=> PHP 8.5 NTS scaffold found.' \
+        ; fi \
+        && if \
+            [ -d "$php_prefix"/php/8.5/zts/ ] \
+            ; then \
+                echo -e '=> PHP 8.5 ZTS scaffold found.' \
+        ; fi \
+    ; fi \
 ; else \
-    echo -e '`/etc/php-php-prefix` NOT found.' \
+    echo -e '`=> /etc/php-php-prefix` NOT found.' \
 ; fi
