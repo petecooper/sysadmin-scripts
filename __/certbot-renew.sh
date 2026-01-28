0db4fac587e74d4764b0e60e3fa62968d01d9a13
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:01:44 +0000

    ~

--- __/certbot-renew.sh
+++ __/certbot-renew.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if \
-[ -f "/opt/certbot/certbot" ] \
+    [ -f "/opt/certbot/certbot" ] \
 ; then
-sudo /opt/certbot/certbot \
-renew \
---config-dir \
-/etc/certbot/ \
-&& exit \
+    sudo /opt/certbot/certbot \
+    renew \
+    --config-dir \
+    /etc/certbot/ \
+    && exit \
 ; fi \
 && if \
-[ -f "/usr/bin/certbot" ] \
+    [ -f "/usr/bin/certbot" ] \
 ; then
-sudo /usr/bin/certbot \
-renew \
---config-dir \
-/etc/certbot/ \
-&& exit \
+    sudo /usr/bin/certbot \
+    renew \
+    --config-dir \
+    /etc/certbot/ \
+    && exit \
 ; fi
