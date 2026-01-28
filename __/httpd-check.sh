7a8d9ee1223d88611bc0e7c4e8732365b12109ff
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:28 +0000

    ~

--- __/httpd-check.sh
+++ __/httpd-check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if \
-[[ -f /usr/sbin/nginx/nginx ]] \
+    [[ -f /usr/sbin/nginx/nginx ]] \
 ; then \
-echo -e '\n=> nginx found at `/usr/sbin/nginx/nginx`.' \
-&& echo -e '=> Checking nginx version...' \
-&& /usr/sbin/nginx/nginx -v \
+    echo -e '\n=> nginx found at `/usr/sbin/nginx/nginx`.' \
+    && echo -e '=> Checking nginx version...' \
+    && /usr/sbin/nginx/nginx -v \
 ; fi \
 && if \
-[[ -f /usr/sbin/nginx ]] \
+    [[ -f /usr/sbin/nginx ]] \
 ; then \
-echo -e '\n=> nginx found at `/usr/sbin/nginx`.' \
-&& echo -e '=> Checking nginx version...' \
-&& /usr/sbin/nginx -v \
+    echo -e '\n=> nginx found at `/usr/sbin/nginx`.' \
+    && echo -e '=> Checking nginx version...' \
+    && /usr/sbin/nginx -v \
 ; fi
