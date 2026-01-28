47355f711548e845923a069a134db5e291ebd501
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:30 +0000

    ~

--- __/httpd-conf-check.sh
+++ __/httpd-conf-check.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 if \
-[[ -f /etc/nginx/nginx.conf ]] \
+    [[ -f /etc/nginx/nginx.conf ]] \
 ; then \
-echo -e '\n=> nginx.conf found at `/etc/nginx/nginx.conf`.' \
+    echo -e '\n=> nginx.conf found at `/etc/nginx/nginx.conf`.' \
 ; fi \
-#!/bin/bash
-if \
-[[ -f /opt/nginx/nginx.conf ]] \
+&& if \
+    [[ -f /opt/nginx/nginx.conf ]] \
 ; then \
-echo -e '\n=> nginx.conf found at `/opt/nginx/nginx.conf`.' \
+    echo -e '\n=> nginx.conf found at `/opt/nginx/nginx.conf`.' \
 ; fi \
-#!/bin/bash
-if \
-[[ -f /opt/nginx/nginx/nginx.conf ]] \
+&& if \
+    [[ -f /opt/nginx/nginx/nginx.conf ]] \
 ; then \
-echo -e '\n=> nginx.conf found at `/opt/nginx/nginx/nginx.conf`.' \
+    echo -e '\n=> nginx.conf found at `/opt/nginx/nginx/nginx.conf`.' \
 ; fi
-
