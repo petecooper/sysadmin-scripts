9b168904213dd91d43b9aa40494c1a75fbe05a45
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:49 +0000

    ~

--- __/nginx-stop.sh
+++ __/nginx-stop.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-if [ -f "/usr/sbin/nginx" ] ; then
-sudo /usr/sbin/nginx -s stop
-fi
-if [ -f "/usr/sbin/nginx/nginx" ] ; then
-sudo /usr/sbin/nginx/nginx -s stop
-fi
+if \
+    [ -f "/usr/sbin/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx -s stop \
+; fi \
+&& if \
+    [ -f "/usr/sbin/nginx/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx/nginx -s stop \
+; fi
