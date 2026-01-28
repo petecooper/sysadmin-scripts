d19e160719b94d76744fa5aa088900a0f33f538b
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:41 +0000

    ~

--- __/nginx-quit.sh
+++ __/nginx-quit.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-if [ -f "/usr/sbin/nginx" ] ; then
-sudo /usr/sbin/nginx -s quit
-fi
-if [ -f "/usr/sbin/nginx/nginx" ] ; then
-sudo /usr/sbin/nginx/nginx -s quit
-fi
+if \
+    [ -f "/usr/sbin/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx -s quit \
+; fi \
+&& if \
+    [ -f "/usr/sbin/nginx/nginx" ] \
+; then \
+    sudo /usr/sbin/nginx/nginx -s quit \
+; fi
