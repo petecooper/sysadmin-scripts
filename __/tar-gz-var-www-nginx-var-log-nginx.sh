65bbecd64852ff9b177305decf6167b1e93086fb
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:03:27 +0000

    ~

--- __/tar-gz-var-www-nginx-var-log-nginx.sh
+++ __/tar-gz-var-www-nginx-var-log-nginx.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-tar cvf - -C "$HOME" /var/www/ $(cat /etc/nginx-nginx-prefix)/nginx/ /var/log/nginx/ | \
-gzip -c9 > $(date +"%Y%m%d_%H%M%S")-$(hostname)-var-www_nginx_var-log-nginx.zip
+tar cvf - \
+-C "$HOME" \
+/var/www/ \
+"$(cat /etc/nginx-nginx-prefix)"/nginx/ \
+/var/log/nginx/ | \
+gzip -c9 > \
+"$(date +"%Y%m%d_%H%M%S")"-"$(hostname)"-var-www_nginx_var-log-nginx.tar.gz
