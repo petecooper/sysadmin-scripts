2ddeb98f59d3cd77c04e091fa38012330901e61f
Pete Cooper <pete@pragmatika.net>
2026-01-28 13:03:36 +0000

    ~

--- __/php-version-check-ng.sh
+++ __/php-version-check-ng.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 if \
-[[ -f /etc/php-php-prefix ]] \
+    [[ -f /etc/php-php-prefix ]] \
 ; then \
     php_prefix="$(< /etc/php-php-prefix)" \
     && echo -e '=> `/etc/php-php-prefix` found.' \
     && if \
-    [ -d "$php_prefix"/php/8.5/ ] \
+        [ -d "$php_prefix"/php/8.5/ ] \
     ; then \
         echo -e '=> PHP 8.5 scaffold found.' \
         && if \
             [ -d "$php_prefix"/php/8.5/nts/ ] \
+        ; then \
+            echo -e '=> PHP 8.5 NTS scaffold found.' \
+            && if \
+                [ -f "$php_prefix"/php/8.5/nts/bin/php ] \
             ; then \
-                echo -e '=> PHP 8.5 NTS scaffold found.' \
+                echo -e '=> PHP 8.5 NTS `php` found.' \
+            ; else \
+                echo -e '=> PHP 8.5 NTS `php` NOT found.' \
+            ; fi \
         ; fi \
         && if \
             [ -d "$php_prefix"/php/8.5/zts/ ] \
+        ; then \
+            echo -e '=> PHP 8.5 ZTS scaffold found.' \
+            && if \
+                [ -f "$php_prefix"/php/8.5/zts/bin/php ] \
             ; then \
-                echo -e '=> PHP 8.5 ZTS scaffold found.' \
+                echo -e '=> PHP 8.5 ZTS `php` found.' \
+            ; else \
+                echo -e '=> PHP 8.5 ZTS `php` NOT found.' \
+            ; fi \
         ; fi \
     ; fi \
 ; else \
