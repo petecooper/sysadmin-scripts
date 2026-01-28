59b2169aa67ed4d1188af0ed2d79cf93f1429f6b
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:00:51 +0000

    ~

--- __/certbot-renew-dry-run.sh
+++ __/certbot-renew-dry-run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 if \
-[ -f "/opt/certbot/certbot" ] \
+    [ -f "/opt/certbot/certbot" ] \
 ; then
-sudo /opt/certbot/certbot \
-renew \
---config-dir \
-/etc/certbot/ \
---dry-run \
-&& exit \
+    sudo /opt/certbot/certbot \
+    renew \
+    --config-dir \
+    /etc/certbot/ \
+    --dry-run \
+    && exit \
 ; fi \
 && if \
-[ -f "/usr/bin/certbot" ] \
+    [ -f "/usr/bin/certbot" ] \
 ; then
-sudo /usr/bin/certbot \
-renew \
---config-dir \
-/etc/certbot/ \
---dry-run \
-&& exit \
+    sudo /usr/bin/certbot \
+    renew \
+    --config-dir \
+    /etc/certbot/ \
+    --dry-run \
+    && exit \
 ; fi
