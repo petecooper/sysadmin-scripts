fb1d881da2c6f8ee0f2a05d43d5d278cf683883f
Pete Cooper <pete@pragmatika.net>
2026-01-28 15:02:39 +0000

    ~

--- __/nginx-prefix-namespace-fix.sh
+++ __/nginx-prefix-namespace-fix.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-if [ -f "/etc/nginx-log-dir-base" ] ; then
-sudo mv \
-/etc/nginx-log-dir-base \
-/etc/nginx-nginx-log-dir-base
-fi
-if [ -f "/etc/nginx-prefix" ] ; then
-sudo mv \
-/etc/nginx-prefix \
-/etc/nginx-nginx-prefix
-fi
-if [ -f "/etc/nginx-tls-vendor" ] ; then
-sudo mv \
-/etc/nginx-tls-vendor \
-/etc/nginx-nginx-tls-vendor
-fi
+if \
+    [ -f "/etc/nginx-log-dir-base" ] \
+; then \
+    sudo mv \
+    /etc/nginx-log-dir-base \
+    /etc/nginx-nginx-log-dir-base \
+; fi \
+&& if \
+    [ -f "/etc/nginx-prefix" ] \
+; then
+    sudo mv \
+    /etc/nginx-prefix \
+    /etc/nginx-nginx-prefix \
+; fi \
+&& if \
+    [ -f "/etc/nginx-tls-vendor" ] \
+; then \
+    sudo mv \
+    /etc/nginx-tls-vendor \
+    /etc/nginx-nginx-tls-vendor \
+; fi
