a406d87e44721e450723738d18ae85a2316adb67
Pete Cooper <pete@pragmatika.net>
2026-06-18 18:15:04 +0100

    ~

--- __/httpd-check.sh
+++ __/httpd-check.sh
@@ -2,32 +2,32 @@
 if \
     [[ -f /usr/sbin/nginx/nginx ]] \
 ; then \
-    echo -e '== nginx ==' \
-    && echo -e '\n=> nginx found at `/usr/sbin/nginx/nginx`.' \
+    echo -e '\n== nginx ==' \
+    && echo -e '=> nginx found at `/usr/sbin/nginx/nginx`.' \
     && echo -e '=> Checking nginx version...' \
     && /usr/sbin/nginx/nginx -v \
 ; fi \
 && if \
     [[ -f /usr/sbin/nginx ]] \
 ; then \
-    echo -e '== nginx ==' \
-    && echo -e '\n=> nginx found at `/usr/sbin/nginx`.' \
+    echo -e '\n== nginx ==' \
+    && echo -e '=> nginx found at `/usr/sbin/nginx`.' \
     && echo -e '=> Checking nginx version...' \
     && /usr/sbin/nginx -v \
 ; fi \
 && if \
     [[ -f /opt/caddy/caddy/caddy ]] \
 ; then \
-    echo -e '== Caddy ==' \
-    && echo -e '\n=> Caddy found at `/opt/caddy/caddy/caddy`.' \
+    echo -e '\n== Caddy ==' \
+    && echo -e '=> Caddy found at `/opt/caddy/caddy/caddy`.' \
     && echo -e '=> Checking Caddy version...' \
     && /opt/caddy/caddy/caddy version \
 ; fi \
 && if \
     [[ -f /opt/frankenphp/frankenphp/frankenphp ]] \
 ; then \
-    echo -e '== FrankenPHP ==' \
-    && echo -e '\n=> FrankenPHP found at `/opt/frankenphp/frankenphp/frankenphp`.' \
+    echo -e '\n== FrankenPHP ==' \
+    && echo -e '=> FrankenPHP found at `/opt/frankenphp/frankenphp/frankenphp`.' \
     && echo -e '=> Checking FrankenPHP version...' \
     && /opt/frankenphp/frankenphp/frankenphp version \
 ; fi
