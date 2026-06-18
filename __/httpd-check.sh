7da5c8baec814cbb4548cbe889c0abd66e96aad5
Pete Cooper <pete@pragmatika.net>
2026-06-18 15:41:33 +0100

    ~

--- __/httpd-check.sh
+++ __/httpd-check.sh
@@ -12,4 +12,18 @@ if \
     echo -e '\n=> nginx found at `/usr/sbin/nginx`.' \
     && echo -e '=> Checking nginx version...' \
     && /usr/sbin/nginx -v \
+; fi \
+&& if \
+    [[ -f /opt/caddy/caddy/caddy ]] \
+; then \
+    echo -e '\n=> Caddy found at `/opt/caddy/caddy/caddy`.' \
+    && echo -e '=> Checking Caddy version...' \
+    && /opt/caddy/caddy/caddy version \
+; fi \
+&& if \
+    [[ -f /opt/frankenphp/frankenphp/frankenphp ]] \
+; then \
+    echo -e '\n=> FrankenPHP found at `/opt/frankenphp/frankenphp/frankenphp`.' \
+    && echo -e '=> Checking FrankenPHP version...' \
+    && /opt/frankenphp/frankenphp/frankenphp version \
 ; fi
